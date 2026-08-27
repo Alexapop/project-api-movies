9d8378ff3ab92588fda1e7b0d46ff7d996d42469
Ioana <ioana3popescu@gmail.com>
2026-08-27 22:21:27 +0200

    feat: add genre entity with generated id and name

---
+++ src/main/java/dev/ioana/apimovies/entity/GenreEntity.java
@@ -0,0 +1,40 @@
+package dev.ioana.apimovies.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "genre")
+public class GenreEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_genre")
+    private Long id;
+
+    @Column(name = "name_genre")
+    private String name;
+
+    public GenreEntity() {
+
+    }
+
+    public GenreEntity(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
